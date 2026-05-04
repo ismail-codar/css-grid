be78eaa219ae03bf4baa5fd5bd0f31c75fef45aa
İsmail Codar <icodar@hotmail.com>
2026-05-04 07:39:02 +0300

    build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
-  sourcemap: true,
+  sourcemap: false,
+  minify: true,
   clean: true,
   external: ['react', 'react-dom', 'react/jsx-runtime'],
 })
